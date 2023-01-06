93b0f228fca982fec979721a40aa6f699d98792d
Marko <livental@gmail.com>
2023-01-06 10:58:15 +0200

    First commit working GUI

---
+++ main.py
@@ -0,0 +1,13 @@
+from Controller import Controller
+
+
+class Hangman:
+
+    def __init__(self):
+        Controller().main()
+
+
+if __name__ == '__main__':
+    # TODO read commandline db name
+    # TODO git
+    Hangman()
